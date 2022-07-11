3105d5c55f9426f0648a4ce679b8212ee4cf0b58
sekosho <sskshogo.10.24@gmail.com>
2022-07-12 07:58:23 +0900

    serverless frameworkユーザを追加。auto-deploy-sesokoから最強権限を外した。

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { v4: uuidv4 } = require('uuid');
 exports.handler = async(event, context, callback) => {
     console.log(uuidv4());
-    console.log("pipeline test");
+    console.log("pipeline test ");
 }
